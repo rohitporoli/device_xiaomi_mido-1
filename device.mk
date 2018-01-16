ad88fcf202717f45c0272e0c261b86dd055d400e
DevStark <devstark15@gmail.com>
2018-01-16 18:34:20 +0000

    mido: Add PixelStyle
    Signed-off-by: rohitporoli <rohitp4all@gmail.com>

--- device.mk
+++ device.mk
@@ -391,5 +391,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
 
 # Doze
+
 PRODUCT_PACKAGES += \
     XiaomiDoze
+# Google Lens
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017.xml:system/etc/sysconfig/pixel_2017.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml
